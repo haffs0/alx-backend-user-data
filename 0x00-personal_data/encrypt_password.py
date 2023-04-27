a0e9928018eb05c6b96d249737124c978ee47890
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-04-27 09:51:00 +0000

    password hashing

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A module for encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Password hashing using random salt.
+    """
+    return bcrypt.hashpw(passwprd.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if is a valid password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
