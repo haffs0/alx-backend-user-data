bbe32cb1beabac25d3910896c5b39ce46b1076b4
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-05-04 11:21:01 +0000

    update session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API.
 """
+import uuid
+
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session authentication class.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create session ID"""
+        if user_id is None:
+            return None
+        if type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
