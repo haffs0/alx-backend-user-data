f6a9f2c27cfd8533b3b71d35170f0dd39874474d
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-05-04 14:26:01 +0000

    session view

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -29,7 +29,7 @@ def login() -> Tuple[str, int]:
     if users[0].is_valid_password('password'):
         from api.v1.app import auth
 
-        session_id = auth.create_session(users[0].id)
+        session_id = auth.create_session(getattr(users[0], 'id'))
         res = jsonify(users[0].to_json())
         res.set_cookie(os.getenv("SESSION_NAME"), session_id)
         return res
