ec31a15c8a1f1f5405470c6bee2c2d01eaaf01e2
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-05-09 09:17:00 +0000

    update method for user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,9 +1,11 @@
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -31,7 +33,45 @@ class DB:
 
     def add_user(self, email, hashed_password) -> User:
         """Add new user to db"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Retrive a user from database by set of filters"""
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+                    tuple_(*fields).in_([tuple(values)])
+                ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user by user_id"""
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_sources = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_sources[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        print(update_sources)
+        self._session.query(User).filter(User.id == user_id).update(
+            update_sources,
+            synchronize_session=False,
+        )
+        self._session.commit()
