acc73f094237eeeef28f8c91ed4df31b04314965
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-05-09 20:56:06 +0000

    add methods

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,3 +84,19 @@ class Auth:
                 return reset_token
         except NoResultFound:
             raise ValueError()
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update user password"""
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        self._db.update_user(
+                    user.id,
+                    hashed_password=_hash_password(password),
+                    reset_token=None
+        )
+        return None
